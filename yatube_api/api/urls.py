35775bec46bbe1823c37898556121bbd5e9da352
Грешилов Станислав Андреевич <GreshilovSA@proletarsky.ru>
2025-09-26 14:41:16 +0300

    Add router, serializer and viewSet for posts

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
 from django.urls import path
 
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, CommentViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+# router.register()
+
 urlpatterns = [
 ]
+
+urlpatterns += router.urls
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,22 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from posts.models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Набор представлений для просмотра, редактирования и удаления
+    данных о публикациях
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Набор представлений для просмотра, редактирования и удаления
+    данных о комментариях
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
